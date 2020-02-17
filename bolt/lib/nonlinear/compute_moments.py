Fixed integral measure in the computation of density

bolt/lib/nonlinear/compute_moments.py
@@ -40,16 +40,22 @@ def compute_moments(self, moment_name, f=None):
         f = self.f
 
     if (self.physical_system.params.p_space_grid == 'cartesian'):
+        
+        measure = (4./(2.*np.pi*self.physical_system.params.h_bar)**2) * self.dp3 * self.dp2 * self.dp1
+        
         moment = af.broadcast(getattr(self.physical_system.moments, 
                                       moment_name
-                                     ), f, p1, p2, p3, self.dp3 * self.dp2 * self.dp1
+                                     ), f, p1, p2, p3, measure
                              )
 
     elif (self.physical_system.params.p_space_grid == 'polar2D'):
+        
+        measure = (4./(2.*np.pi*self.physical_system.params.h_bar)**2) * self.dp2 * self.dp1
+        
         moment = self.physical_system.params.initial_mu * \
                  af.broadcast(getattr(self.physical_system.moments, 
                                       moment_name
-                                     ), f, p1, p2, p3, self.dp3 * self.dp2 * self.dp1
+                                     ), f, p1, p2, p3, measure
                              )
 
     else: