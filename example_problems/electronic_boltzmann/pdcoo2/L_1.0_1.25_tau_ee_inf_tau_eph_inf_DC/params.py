dbbc9f1ff6ef4776f71100d1032946f7028e028d
gitansh95 <gitansh95@gmail.com>
2020-03-29 10:46:50 -0400

    Fixed indices issue, added test funcs, working version

--- example_problems/electronic_boltzmann/pdcoo2/L_1.0_1.25_tau_ee_inf_tau_eph_inf_DC/params.py
+++ example_problems/electronic_boltzmann/pdcoo2/L_1.0_1.25_tau_ee_inf_tau_eph_inf_DC/params.py
@@ -165,10 +165,65 @@ def band_velocity(p1, p2):
 
     return(upper_band_velocity)
 
+def normal_to_circle_unit_vec(theta):
+
+    vel_x = 0.*theta
+    vel_y = 0.*theta
+
+    # (1)
+    start_theta = 0.0
+    end_theta   = np.pi/3
+    mid_theta   = 0.5*(end_theta + start_theta)
+    indices = ((theta >= start_theta) & (theta < end_theta))
+    vel_x = (1-indices)*vel_x + indices*af.cos(theta)
+    vel_y = (1-indices)*vel_y + indices*af.sin(theta)
+
+    # (2)    
+    start_theta = np.pi/3
+    end_theta   = 2*np.pi/3
+    mid_theta   = 0.5*(end_theta + start_theta)
+    indices = ((theta >= start_theta) & (theta < end_theta))
+    vel_x = (1-indices)*vel_x + indices*af.cos(theta)
+    vel_y = (1-indices)*vel_y + indices*af.sin(theta)
+
+    # (3)    
+    start_theta = 2*np.pi/3
+    end_theta   = np.pi
+    mid_theta   = 0.5*(end_theta + start_theta)
+    indices = ((theta >= start_theta) & (theta < end_theta))
+    vel_x = (1-indices)*vel_x + indices*af.cos(theta)
+    vel_y = (1-indices)*vel_y + indices*af.sin(theta)
+    
+    # (4)
+    start_theta = -np.pi
+    end_theta   = -2*np.pi/3
+    mid_theta   = 0.5*(end_theta + start_theta)
+    indices = ((theta >= start_theta) & (theta < end_theta))
+    vel_x = (1-indices)*vel_x + indices*af.cos(theta)
+    vel_y = (1-indices)*vel_y + indices*af.sin(theta)
+    
+    # (5)
+    start_theta = -2*np.pi/3
+    end_theta   = -np.pi/3
+    mid_theta   = 0.5*(end_theta + start_theta)
+    indices = ((theta >= start_theta) & (theta < end_theta))
+    vel_x = (1-indices)*vel_x + indices*af.cos(theta)
+    vel_y = (1-indices)*vel_y + indices*af.sin(theta)
+
+    # (6)
+    start_theta = -np.pi/3
+    end_theta   = 0
+    mid_theta   = 0.5*(end_theta + start_theta)
+    indices = ((theta >= start_theta) & (theta < end_theta))
+    vel_x = (1-indices)*vel_x + indices*af.cos(theta)
+    vel_y = (1-indices)*vel_y + indices*af.sin(theta)
+
+    return ([vel_x, vel_y]) 
+
 def normal_to_hexagon_unit_vec(theta):
 
-    vel_x = np.zeros(theta.shape)
-    vel_y = np.zeros(theta.shape)
+    vel_x = 0.*theta
+    vel_y = 0.*theta
 
 #
 #           2*pi/3                 pi/3
@@ -195,52 +250,48 @@ def normal_to_hexagon_unit_vec(theta):
     end_theta   = np.pi/3
     mid_theta   = 0.5*(end_theta + start_theta)
     indices = ((theta >= start_theta) & (theta < end_theta))
-    vel_x[indices] = np.cos(mid_theta)
-    vel_y[indices] = np.sin(mid_theta)
+    vel_x = (1-indices)*vel_x + indices*np.cos(mid_theta)
+    vel_y = (1-indices)*vel_y + indices*np.sin(mid_theta)
 
     # (2)    
     start_theta = np.pi/3
     end_theta   = 2*np.pi/3
     mid_theta   = 0.5*(end_theta + start_theta)
     indices = ((theta >= start_theta) & (theta < end_theta))
-    vel_x[indices] = np.cos(mid_theta)
-    vel_y[indices] = np.sin(mid_theta)
+    vel_x = (1-indices)*vel_x + indices*np.cos(mid_theta)
+    vel_y = (1-indices)*vel_y + indices*np.sin(mid_theta)
 
     # (3)    
     start_theta = 2*np.pi/3
     end_theta   = np.pi
     mid_theta   = 0.5*(end_theta + start_theta)
     indices = ((theta >= start_theta) & (theta < end_theta))
-    vel_x[indices] = np.cos(mid_theta)
-    vel_y[indices] = np.sin(mid_theta)
+    vel_x = (1-indices)*vel_x + indices*np.cos(mid_theta)
+    vel_y = (1-indices)*vel_y + indices*np.sin(mid_theta)
     
     # (4)
     start_theta = -np.pi
     end_theta   = -2*np.pi/3
     mid_theta   = 0.5*(end_theta + start_theta)
     indices = ((theta >= start_theta) & (theta < end_theta))
-    vel_x[indices] = np.cos(mid_theta)
-    vel_y[indices] = np.sin(mid_theta)
+    vel_x = (1-indices)*vel_x + indices*np.cos(mid_theta)
+    vel_y = (1-indices)*vel_y + indices*np.sin(mid_theta)
     
     # (5)
     start_theta = -2*np.pi/3
     end_theta   = -np.pi/3
     mid_theta   = 0.5*(end_theta + start_theta)
     indices = ((theta >= start_theta) & (theta < end_theta))
-    vel_x[indices] = np.cos(mid_theta)
-    vel_y[indices] = np.sin(mid_theta)
+    vel_x = (1-indices)*vel_x + indices*np.cos(mid_theta)
+    vel_y = (1-indices)*vel_y + indices*np.sin(mid_theta)
 
     # (6)
     start_theta = -np.pi/3
     end_theta   = 0
     mid_theta   = 0.5*(end_theta + start_theta)
     indices = ((theta >= start_theta) & (theta < end_theta))
-    vel_x[indices] = np.cos(mid_theta)
-    vel_y[indices] = np.sin(mid_theta)
-
-    # Convert to arrayfire arrays
-    vel_x = af.from_ndarray(vel_x)
-    vel_y = af.from_ndarray(vel_y)
+    vel_x = (1-indices)*vel_x + indices*np.cos(mid_theta)
+    vel_y = (1-indices)*vel_y + indices*np.sin(mid_theta)
 
     return ([vel_x, vel_y]) 
 
