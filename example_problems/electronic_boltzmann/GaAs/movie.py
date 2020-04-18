26053f6592b961ed3de66a455f3ab1e09f17c4c4
gitansh95 <gitansh95@gmail.com>
2020-04-18 19:49:33 -0400

    movie.py cleanup

--- example_problems/electronic_boltzmann/GaAs/movie.py
+++ example_problems/electronic_boltzmann/GaAs/movie.py
@@ -74,19 +74,8 @@ q2 = domain.q2_start + (0.5 + np.arange(N_q2)) * (domain.q2_end - domain.q2_star
 
 q2_meshgrid, q1_meshgrid = np.meshgrid(q2, q1)
 
-#X = q1_meshgrid
-#Y = q2_meshgrid
-
-#q1_meshgrid = af.from_ndarray(q1_meshgrid)
-#q2_meshgrid = af.from_ndarray(q2_meshgrid)
-#
-#x, y = coords.get_cartesian_coords_for_post(q1_meshgrid, q2_meshgrid)
-#
-#x = x.to_ndarray()
-#y = y.to_ndarray()
-
 coords = io.readBinaryFile("coords.bin")
-coords = coords[0].reshape(N_q2, N_q1, 7)
+coords = coords[0].reshape(N_q2, N_q1, 13)
     
 x = coords[:, :, 0].T
 y = coords[:, :, 1].T
@@ -110,8 +99,6 @@ lagrange_multiplier_files = \
 print ("moment files : ", moment_files.size)
 print ("lagrange multiplier files : ", lagrange_multiplier_files.size)
 
-dt = 0.025/8#params.dt
-#dump_interval = params.dump_steps
 
 time_array = np.loadtxt(filepath+"/dump_time_array.txt")
 
@@ -141,60 +128,60 @@ for file_number, dump_file in enumerate(moment_files[:]):
     print ("x.shape : ", x.shape)    
 
     plot_grid(x[::5, ::5], y[::5, ::5], alpha=0.5)
-    #pl.contourf(x, y, density.T, 100, cmap='bwr')
+    pl.contourf(x, y, density.T, 100, cmap='bwr')
     pl.title(r'Time = ' + "%.2f"%(time_array[file_number]) + " ps")
     #pl.colorbar()
     
 
-#    # Plot streamlines for domains 3 and 9
-#    i_start = 188
-#    i_end   = 605-142-29-27
-#    d = 5*(i_end - i_start)/N_q1
-#    print (d)
-#    pl.streamplot(x[i_start:i_end, 0], y[i_start, :], 
-#                  vel_drift_x[:, i_start:i_end], vel_drift_y[:, i_start:i_end],
-#                  density=d, color='k',
-#                  linewidth=0.7, arrowsize=1
-#                 )
-#
-#    # Plot streamlines for domains 1 and 7
-#    i_start = 0
-#    i_end   = 150
-#    d = 5*(i_end - i_start)/N_q1
-#    print (d)
-#    pl.streamplot(x[i_start:i_end, 0], y[i_start, :], 
-#                  vel_drift_x[:, i_start:i_end], vel_drift_y[:, i_start:i_end],
-#                  density=d, color='k',
-#                  linewidth=0.7, arrowsize=1
-#                 )
-#
-#    # Plot streamlines for domains 6 and 12
-#    i_start = 605-142
-#    i_end   = 605
-#    d = 5*(i_end - i_start)/N_q1
-#    print (d)
-#    pl.streamplot(x[i_start:i_end, 0], y[i_start, :100], 
-#                  vel_drift_x[:100, i_start:i_end], vel_drift_y[:100, i_start:i_end],
-#                  density=0.58*d, color='k',
-#                  linewidth=0.7, arrowsize=1
-#                 )
-#    pl.streamplot(x[i_start:i_end, 0], y[i_start, 100:], 
-#                  vel_drift_x[100:, i_start:i_end], vel_drift_y[100:, i_start:i_end],
-#                  density=0.4*d, color='k',
-#                  linewidth=0.7, arrowsize=1
-#                 )
-#
-#
-#    # Plot streamlines for domain 4
-#    i_start = 605-142-29-27
-#    i_end   = 605-142-29
-#    d = 2*5*(i_end - i_start)/N_q1
-#    print (d)
-#    pl.streamplot(x[i_start:i_end, 0], y[i_start, :100], 
-#                  vel_drift_x[:100, i_start:i_end], vel_drift_y[:100, i_start:i_end],
-#                  density=d, color='k',
-#                  linewidth=0.7, arrowsize=1
-#                 )
+    # Plot streamlines for domains 3 and 9
+    i_start = 188
+    i_end   = 605-142-29-27
+    d = 5*(i_end - i_start)/N_q1
+    print (d)
+    pl.streamplot(x[i_start:i_end, 0], y[i_start, :], 
+                  vel_drift_x[:, i_start:i_end], vel_drift_y[:, i_start:i_end],
+                  density=d, color='k',
+                  linewidth=0.7, arrowsize=1
+                 )
+
+    # Plot streamlines for domains 1 and 7
+    i_start = 0
+    i_end   = 150
+    d = 5*(i_end - i_start)/N_q1
+    print (d)
+    pl.streamplot(x[i_start:i_end, 0], y[i_start, :], 
+                  vel_drift_x[:, i_start:i_end], vel_drift_y[:, i_start:i_end],
+                  density=d, color='k',
+                  linewidth=0.7, arrowsize=1
+                 )
+
+    # Plot streamlines for domains 6 and 12
+    i_start = 605-142
+    i_end   = 605
+    d = 5*(i_end - i_start)/N_q1
+    print (d)
+    pl.streamplot(x[i_start:i_end, 0], y[i_start, :100], 
+                  vel_drift_x[:100, i_start:i_end], vel_drift_y[:100, i_start:i_end],
+                  density=0.58*d, color='k',
+                  linewidth=0.7, arrowsize=1
+                 )
+    pl.streamplot(x[i_start:i_end, 0], y[i_start, 100:], 
+                  vel_drift_x[100:, i_start:i_end], vel_drift_y[100:, i_start:i_end],
+                  density=0.4*d, color='k',
+                  linewidth=0.7, arrowsize=1
+                 )
+
+
+    # Plot streamlines for domain 4
+    i_start = 605-142-29-27
+    i_end   = 605-142-29
+    d = 2*5*(i_end - i_start)/N_q1
+    print (d)
+    pl.streamplot(x[i_start:i_end, 0], y[i_start, :100], 
+                  vel_drift_x[:100, i_start:i_end], vel_drift_y[:100, i_start:i_end],
+                  density=d, color='k',
+                  linewidth=0.7, arrowsize=1
+                 )
     
     #pl.xlim([-1, 27])
     #pl.ylim([q2[0], q2[-1]])
