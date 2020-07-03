9695d02df1ba8961f13ca5ba9468e5d9535faec5
gitansh95 <gitansh95@gmail.com>
2020-07-03 19:00:55 +0000

    Added variables for the 4 vertices of the circle in coords.py

--- example_problems/electronic_boltzmann/circular_domain/coords.py
+++ example_problems/electronic_boltzmann/circular_domain/coords.py
@@ -13,6 +13,9 @@ def get_cartesian_coords(q1, q2,
     q1_midpoint = 0.5*(af.max(q1) + af.min(q1))
     q2_midpoint = 0.5*(af.max(q2) + af.min(q2))
 
+    d_q1          = (q1[0, 0, 1, 0] - q1[0, 0, 0, 0]).scalar()
+    d_q2          = (q2[0, 0, 0, 1] - q2[0, 0, 0, 0]).scalar()
+
     # Default initializsation to rectangular grid
     x = q1
     y = q2
@@ -20,6 +23,15 @@ def get_cartesian_coords(q1, q2,
     # Radius and center of circular region
     radius          = 0.5
     center          = [0, 0]
+
+    shift_x = 1.*d_q1/2
+    shift_y = 1.*d_q2/2
+
+    x_y_circle_top_left     = [-radius/np.sqrt(2) + shift_x, radius/np.sqrt(2)  - shift_y]
+    x_y_circle_bottom_left  = [-radius/np.sqrt(2) + shift_x, -radius/np.sqrt(2) + shift_y]
+    x_y_circle_top_right    = [radius/np.sqrt(2)  - shift_x, radius/np.sqrt(2)  - shift_y]
+    x_y_circle_bottom_right = [radius/np.sqrt(2)  - shift_x, -radius/np.sqrt(2) + shift_y]
+
 
     if (q1_start_local_left != None and q2_start_local_bottom != None):
 
@@ -36,9 +48,9 @@ def get_cartesian_coords(q1, q2,
             x_y_left_center  = [-radius/np.sqrt(2), (-1-radius/np.sqrt(2))/2]
             x_y_right_center = [ radius/np.sqrt(2), (-1-radius/np.sqrt(2))/2]
             
-            x_y_top_left     = [-radius/np.sqrt(2), -radius/np.sqrt(2)]
+            x_y_top_left     = x_y_circle_bottom_left
             x_y_top_center   = [0                 , -radius           ]
-            x_y_top_right    = [radius/np.sqrt(2) , -radius/np.sqrt(2)]     
+            x_y_top_right    = x_y_circle_bottom_right
 
             x, y, jacobian = quadratic(q1, q2,
                                        x_y_bottom_left,   x_y_bottom_right,
@@ -62,7 +74,7 @@ def get_cartesian_coords(q1, q2,
             
             x_y_top_left     = [-1,                        -radius/np.sqrt(2)]
             x_y_top_center   = [-(1.+radius/np.sqrt(2))/2, -radius/np.sqrt(2)]
-            x_y_top_right    = [-radius/np.sqrt(2),        -radius/np.sqrt(2)] 
+            x_y_top_right    = x_y_circle_bottom_left
     
             x, y, jacobian = quadratic(q1, q2,
                                        x_y_bottom_left,   x_y_bottom_right,
@@ -84,7 +96,7 @@ def get_cartesian_coords(q1, q2,
             x_y_left_center   = [ radius/np.sqrt(2), (-1-radius/np.sqrt(2))/2]
             x_y_right_center  = [1,                  (-1-radius/np.sqrt(2))/2]
             
-            x_y_top_left     = [radius/np.sqrt(2),         -radius/np.sqrt(2)]
+            x_y_top_left     = x_y_circle_bottom_right
             x_y_top_center   = [(1.+radius/np.sqrt(2))/2,  -radius/np.sqrt(2)]
             x_y_top_right    = [1,                         -radius/np.sqrt(2)] 
     
@@ -101,9 +113,9 @@ def get_cartesian_coords(q1, q2,
         # Top-center domain
         elif ((q2_midpoint > 0.33) and (q1_midpoint > -0.33) and (q1_midpoint < 0.33)):
 
-            x_y_bottom_left   = [-radius/np.sqrt(2), radius/np.sqrt(2)]
+            x_y_bottom_left   = x_y_circle_top_left
             x_y_bottom_center = [0,                  radius]
-            x_y_bottom_right  = [radius/np.sqrt(2),  radius/np.sqrt(2)]
+            x_y_bottom_right  = x_y_circle_top_right
             
             x_y_left_center   = [-radius/np.sqrt(2), (1+radius/np.sqrt(2))/2]
             x_y_right_center  = [ radius/np.sqrt(2), (1+radius/np.sqrt(2))/2]
@@ -127,7 +139,7 @@ def get_cartesian_coords(q1, q2,
 
             x_y_bottom_left   = [-1,                         radius/np.sqrt(2)]
             x_y_bottom_center = [-(1.+radius/np.sqrt(2))/2,  radius/np.sqrt(2)]
-            x_y_bottom_right  = [-radius/np.sqrt(2),         radius/np.sqrt(2)]
+            x_y_bottom_right  = x_y_circle_top_left
             
             x_y_left_center  = [-1,                 (1+radius/np.sqrt(2))/2]
             x_y_right_center = [-radius/np.sqrt(2), (1+radius/np.sqrt(2))/2]
@@ -149,7 +161,7 @@ def get_cartesian_coords(q1, q2,
         # Top-right domain
         elif ((q2_midpoint > 0.33) and (q1_midpoint > 0.33) and (q1_midpoint < 1)):
 
-            x_y_bottom_left   = [radius/np.sqrt(2),         radius/np.sqrt(2)]
+            x_y_bottom_left   = x_y_circle_top_right
             x_y_bottom_center = [(1.+radius/np.sqrt(2))/2,  radius/np.sqrt(2)]
             x_y_bottom_right  = [1,                         radius/np.sqrt(2)]
             
@@ -173,14 +185,14 @@ def get_cartesian_coords(q1, q2,
         # Center-Right domain
         elif ((q2_midpoint > -0.33) and (q2_midpoint < 0.33) and (q1_midpoint > 0.33)):
 
-            x_y_bottom_left   = [radius/np.sqrt(2),           -radius/np.sqrt(2)]
+            x_y_bottom_left   = x_y_circle_bottom_right
             x_y_bottom_center = [(1.+radius/np.sqrt(2))/2,    -radius/np.sqrt(2)]
             x_y_bottom_right  = [1.,                          -radius/np.sqrt(2)]
             
             x_y_left_center  = [radius, 0.]
             x_y_right_center = [1.,     0.]
             
-            x_y_top_left     = [radius/np.sqrt(2),           radius/np.sqrt(2) ]
+            x_y_top_left     = x_y_circle_top_right
             x_y_top_center   = [(1.+radius/np.sqrt(2))/2,    radius/np.sqrt(2) ]
             x_y_top_right    = [1.,                          radius/np.sqrt(2) ]
             
@@ -199,14 +211,14 @@ def get_cartesian_coords(q1, q2,
 
             x_y_bottom_left   = [-1.,                          -radius/np.sqrt(2)]
             x_y_bottom_center = [-(1.+radius/np.sqrt(2))/2,    -radius/np.sqrt(2)]
-            x_y_bottom_right  = [-radius/np.sqrt(2),           -radius/np.sqrt(2)  ]
+            x_y_bottom_right  = x_y_circle_bottom_left
             
             x_y_left_center  = [-1.,     0.]
             x_y_right_center = [-radius, 0.]
             
             x_y_top_left     = [-1.,                          radius/np.sqrt(2)]
             x_y_top_center   = [-(1.+radius/np.sqrt(2))/2,    radius/np.sqrt(2)]
-            x_y_top_right    = [-radius/np.sqrt(2),           radius/np.sqrt(2)  ]
+            x_y_top_right    = x_y_circle_top_left
             
             x, y, jacobian = quadratic(q1, q2,
                                        x_y_bottom_left,   x_y_bottom_right,
@@ -221,16 +233,16 @@ def get_cartesian_coords(q1, q2,
         # Center domain
         elif ((q2_midpoint > -0.33) and (q2_midpoint < 0.33) and (q1_midpoint > -0.33) and (q1_midpoint < 0.33)):
 
-            x_y_bottom_left   = [-radius/np.sqrt(2),  -radius/np.sqrt(2)]
+            x_y_bottom_left   = x_y_circle_bottom_left
             x_y_bottom_center = [0.,                  -radius]
-            x_y_bottom_right  = [radius/np.sqrt(2),   -radius/np.sqrt(2)]
+            x_y_bottom_right  = x_y_circle_bottom_right
             
             x_y_left_center  = [-radius, 0]
             x_y_right_center = [ radius, 0]
             
-            x_y_top_left     = [-radius/np.sqrt(2),  radius/np.sqrt(2)]
+            x_y_top_left     = x_y_circle_top_left
             x_y_top_center   = [0.,                  radius]
-            x_y_top_right    = [radius/np.sqrt(2),   radius/np.sqrt(2)]
+            x_y_top_right    = x_y_circle_top_right
             
             x, y, jacobian = quadratic(q1, q2,
                                        x_y_bottom_left,   x_y_bottom_right,
